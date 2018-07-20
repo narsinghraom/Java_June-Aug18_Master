a007019cb22d011bc7f40422b5ac5807ef78a298
sumanth03 <41418849+sumanth03@users.noreply.github.com>
2018-07-20 12:19:55 +0530

    Basic file

---
+++ src/com/vtalent/sumanth/Sumanth.java
@@ -0,0 +1,10 @@
+package com.vtalent.sumanth;
+
+public class Sumanth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Sumanth");
+	}
+
+}
