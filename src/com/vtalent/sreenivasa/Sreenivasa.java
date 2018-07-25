8edb01fef8a34f27c36e4eb6bb53b180d3aff74e
SreenivasaReddy12 <40225605+SreenivasaReddy12@users.noreply.github.com>
2018-07-25 11:47:27 +0530

    Test it code

--- src/com/vtalent/sreenivasa/Sreenivasa.java
+++ src/com/vtalent/sreenivasa/Sreenivasa.java
@@ -1,9 +1,22 @@
 package com.vtalent.sreenivasa;
 
-public class Sreenivasa {
+public class Sreenivasa 
+{
+	
+	String str;
+	char ch=str.charAt(0);
+	void disp() 
+	{
+		System.out.println(ch);
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		/*String str;
+		char ch=str.charAt(0);*/
+		Sreenivasa s=new Sreenivasa();
+		s.disp();
 System.out.println("hiii");
 	}
 
