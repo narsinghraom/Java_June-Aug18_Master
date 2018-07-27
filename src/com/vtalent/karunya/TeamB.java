f497fb9734dfdc70f8b5552ff52e825081c15584
karunya <karunya.vtalant@gmail.com>
2018-07-27 10:01:20 +0530

    Test it code

---
+++ src/com/vtalent/karunya/TeamB.java
@@ -0,0 +1,11 @@
+package com.vtalent.karunya;
+
+public class TeamB 
+{
+	public static void main(String[] args)
+	{
+		String text="abc";
+		text=text+text;
+		System.out.println(text);
+	}
+}
---
+++ src/com/vtalent/karunya/Test1.java
@@ -0,0 +1,9 @@
+package com.vtalent.karunya;
+
+public class Test1 {
+        public static void main(String[] args){
+        	String test="abc";
+        	test=test+test;
+            System.out.println(test);
+        }
+}
