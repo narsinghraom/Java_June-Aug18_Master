a9eb3363eb8c8ef90a4720314001414f209bfbfe
RanjitKumarParsa <40783735+RanjitKumarParsa@users.noreply.github.com>
2018-08-01 11:17:28 +0530

    Member Inner Class

---
+++ src/com/vtalent/ranjith/innerClasses/memberInnerClass/OuterEx.java
@@ -0,0 +1,27 @@
+package com.vtalent.ranjith.innerClasses.memberInnerClass;
+
+public class OuterEx {
+private int i=10;
+public void function(int i){
+	System.out.println(this.i+i);//15
+}
+class InnerEx{
+	float f=1.5f;
+	void m1(int k){
+		System.out.println(k);
+	}
+	 void function(float f){
+		 OuterEx e=new OuterEx();
+		e.function((int)f);
+		 //function((int)f);
+		System.out.println(this.f+f);
+	}
+}
+public static void main(String[] args) {
+	/*OuterEx oe=new OuterEx();	
+	OuterEx.InnerEx ie=oe.new InnerEx();*/
+	//.....(or).......
+	OuterEx.InnerEx ie=new OuterEx().new InnerEx();
+	ie.function(2.5f);
+}
+}
