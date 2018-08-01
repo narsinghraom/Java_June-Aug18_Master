e7d93e871da2ae5cf0415ec26f9744704a4a2f50
SreenivasaReddy12 <40225605+SreenivasaReddy12@users.noreply.github.com>
2018-08-01 11:22:54 +0530

    examp

---
+++ src/com/vtalent/sreenivasa/OuterExample.java
@@ -0,0 +1,30 @@
+package com.vtalent.sreenivasa;
+
+public class OuterExample 
+{
+	private int i=20;
+	public void function(int i)
+	{
+		System.out.println(this.i+i);
+	}
+	class InnerExample
+	{
+		float f1=1.5f;
+		public void function(float f1) 
+		{
+			OuterExample o=new OuterExample();
+			o.function((int)f1);
+			System.out.println(this.f1+f1);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*OuterExample oe=new OuterExample();
+OuterExample.InnerExample oe1=oe.new InnerExample();*/
+		//or
+		OuterExample.InnerExample oe1=new OuterExample(). new InnerExample();
+oe1.function(2.5f);
+	}
+
+}
