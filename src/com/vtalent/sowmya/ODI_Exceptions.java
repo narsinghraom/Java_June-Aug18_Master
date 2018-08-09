2d51a905091a155297c8ee8436e42de2586175e5
yerramsowmya <yerramsowmya1994@gmail.com>
2018-08-09 11:06:22 +0530

    ODI EXCEPTIONS
    TESI IT NOW

--- src/com/vtalent/sowmya/ODI_Exceptions.java
+++ src/com/vtalent/sowmya/ODI_Exceptions.java
@@ -11,22 +11,18 @@ public class ODI_Exceptions {
 				return "Match is canceld due to the rain";
 			}
 		}	
-		class OversException1 extends Exception{
-//			private static final long serialVersionUID = 1L;
+		class OversException1 extends Exception{		
 			public String toString(){
 				return "Overs can be<=50";
 			}
 		}
 		class OversException2 extends Exception{
-//			private static final long serialVersionUID = 1L;
 			public String toString(){
 				return "Overs can be<=40";
 			}
 		}
 		class MatchTieException extends Exception{		
-//			private static final long serialVersionUID = 1L;
-
-			public String toString(){
+        public String toString(){
 				return "Match is tie";
 			}
 		}
