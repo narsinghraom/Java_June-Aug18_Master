21ecefb8a311f65a477e7cea318efe561e399e5b
saikrishna <saikrishna6033.vtalent@gmail.com>
2018-08-09 11:12:51 +0530

    SOP statement

---
+++ src/com/vtalent/saikrishna/system/MyTest.java
@@ -0,0 +1,11 @@
+package com.vtalent.saikrishna.system;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=20;
+		MySystem.out.println(i);
+	}
+
+}
