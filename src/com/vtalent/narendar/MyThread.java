f41d99b618acd4e1e1ed91ee77a993e401d37023
narendar478 <narendernalla478@gmail.com>
2018-08-17 10:13:26 +0530

    Threads

--- src/com/vtalent/narendar/MyThread.java
+++ src/com/vtalent/narendar/MyThread.java
@@ -8,9 +8,11 @@ public class MyThread implements Runnable
 
 	public void run()
 	{
-		for(int i=0;i<=3;i++)
+		for(int i=0;i<=5;i++)
 		{
-			System.out.println(i);
+			//System.out.println(i);
+			System.out.println(Thread.currentThread().getName()+i);
+			
 		}
 	}
 	public static void main(String[] args)
@@ -19,9 +21,11 @@ public class MyThread implements Runnable
 	MyThread mt=new MyThread();
 	Thread t=new Thread(mt);
 	//mt.start();
+	t.setName("xyz");
 	t.start();
 	MyThread mt1=new MyThread();
 	Thread t1=new Thread(mt1);
+	t1.setName("abc");
 	//mt1.start();
 	t1.start();
 	
