8bbc315fa603bce65479df7c5ea8f78f7864e3c8
ashwinipakkeeru <ashwinireddy.vtalent@gmail.com>
2018-08-31 12:00:20 +0530

    ashwini:changes

--- src/com/vtalent/ashwini/basicjava/Demo3.java
+++ src/com/vtalent/ashwini/basicjava/Demo3.java
@@ -4,7 +4,7 @@ public class Demo3 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("this is demo 3");
+		System.out.println("this is demo 3 practising for second time");
 
 	}
 
