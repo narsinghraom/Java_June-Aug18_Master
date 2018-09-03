1a4af25af1b54da4c4255efce470b2e37c1604c6
dvhprasad <dvhprasadvtalent@gmail.com>
2018-09-03 11:46:27 +0530

    3 constructors
    ConstructorExamples

---
+++ src/com/vtalent/dvhprasad/basicjava/ALoop.java
@@ -0,0 +1,10 @@
+package com.vtalent.dvhprasad.basicjava;
+
+public class ALoop {
+int i;
+//ALoop al=new ALoop();
+public static void main(String[] args) {
+	ALoop al=new ALoop();
+	System.out.println(al.i);
+}
+}
--- src/com/vtalent/dvhprasad/basicjava/Pattern1.java
+++ src/com/vtalent/dvhprasad/basicjava/Pattern1.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	for(int j=1;j<=i;j++)	
 	{
 	System.out.print(i);
-	}f
+	}
 System.out.println();
 }
   
