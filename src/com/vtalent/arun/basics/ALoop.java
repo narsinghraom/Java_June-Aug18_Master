e5b82ae6764eee63d166071a172470ce8e7f7b3a
Arun225 <Arun225.vtalent@gmail.com>
2018-09-03 11:27:14 +0530

    ArrayLoopExample

---
+++ src/com/vtalent/arun/basics/ALoop.java
@@ -0,0 +1,12 @@
+package com.vtalent.arun.basics;
+
+public class ALoop {
+		int i;
+		ALoop al=new ALoop();
+		public static void main(String[] args) 
+		{
+		ALoop a1=new ALoop();
+		System.out.println(a1.i);
+		}
+	}
+
