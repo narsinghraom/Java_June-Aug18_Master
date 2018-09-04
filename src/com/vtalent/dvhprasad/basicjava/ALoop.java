ee94e80d58bda5f7edd01592b5849cbdfafc670f
dvhprasad <dvhprasadvtalent@gmail.com>
2018-09-04 19:48:18 +0530

    Loop
    due o the object recursion

--- src/com/vtalent/dvhprasad/basicjava/ALoop.java
+++ src/com/vtalent/dvhprasad/basicjava/ALoop.java
@@ -2,9 +2,9 @@ package com.vtalent.dvhprasad.basicjava;
 
 public class ALoop {
 int i;
-//ALoop al=new ALoop();
+ALoop al=new ALoop();
 public static void main(String[] args) {
-	ALoop al=new ALoop();
+	ALoop al=new ALoop();//stack over flow due to the object recursion
 	System.out.println(al.i);
 }
 }
