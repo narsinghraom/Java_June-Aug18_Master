51a2b868ec6dad48cf7d36fac8cac6e9f56331df
yerramsowmya <yerramsowmya1994@gmail.com>
2018-09-05 10:13:59 +0530

    test8

---
+++ src/com/vtalent/sowmya/MySystem.java
@@ -0,0 +1,14 @@
+package com.vtalent.sowmya;
+
+public class MySystem {
+	private MySystem()
+	{
+	}
+	static MyPrintStream out=new MyPrintStream(new MyOutputStream1(){
+		public void console(){
+	}
+
+	
+
+});
+}
