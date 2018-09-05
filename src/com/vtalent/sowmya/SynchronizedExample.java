9c970c1a1fa5e0753c6c3c70f5ab0127d0cc5682
yerramsowmya <yerramsowmya1994@gmail.com>
2018-09-05 10:11:06 +0530

    test2

---
+++ src/com/vtalent/sowmya/SynchronizedExample.java
@@ -0,0 +1,25 @@
+package com.vtalent.sowmya;
+
+public class SynchronizedExample extends Thread {
+	public synchronized void function(int i)
+	{
+		System.out.println(i);
+	}
+	public void run(){
+		for(int i=0;i<3;i++)
+		{
+			function(i);
+		}
+	}
+	public static void main(String[] args) {
+		SynchronizedExample se=new SynchronizedExample();
+		se.start();
+		SynchronizedExample se1=new SynchronizedExample();
+		se1.start();
+		
+		
+		
+		
+	}
+
+}
