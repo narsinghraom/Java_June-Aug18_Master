38d9b5fa7650ac88c861984c12978ce04aa59e39
yerramsowmya <yerramsowmya1994@gmail.com>
2018-09-05 10:14:28 +0530

    test9

---
+++ src/com/vtalent/sowmya/MyPrintStream.java
@@ -0,0 +1,18 @@
+package com.vtalent.sowmya;
+
+public class MyPrintStream {
+	MyPrintStream(MyOutputStream1 outstream)
+	{
+}
+
+public void println(int i)
+{
+	System.out.println(i);
+}
+public void println(String str)
+{
+	System.out.println(str);
+}
+}
+
+
