cc8780fd6b71416ffccecf218970d86491fff300
nagraj001 <nagk785@gmail.com>
2018-09-07 12:06:22 +0530

    basic program

--- src/com/vtalent/nagaraju/basics/Hello.java
+++ src/com/vtalent/nagaraju/basics/Hello.java
@@ -3,6 +3,7 @@ package com.vtalent.nagaraju.basics;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("helloworld2");
 	}
 	
 	
