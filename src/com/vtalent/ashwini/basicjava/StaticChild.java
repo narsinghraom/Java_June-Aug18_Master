78cdd22ed6b5df23c0e947215e1f574907ad42d5
ashwinipakkeeru <ashwinireddy.vtalent@gmail.com>
2018-09-17 09:02:59 +0530

    about static is inherited or not
    sir how can i say  main method of parent call is used by child class ....................can i sai main method is inherited to the child class

---
+++ src/com/vtalent/ashwini/basicjava/StaticChild.java
@@ -0,0 +1,17 @@
+package com.vtalent.ashwini.basicjava;
+
+
+ class StaticParent 
+{
+	public static void main(String[] args)
+	{
+	System.out.println("this is saticparent");
+
+	}
+
+}
+public class StaticChild extends StaticParent
+{
+	
+}
+
